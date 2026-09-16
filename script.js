cb1d7c382fc9d813f65b5df9f46585f91da1e8f6
xbalancedvpn <56534826+xbalancedvpn@users.noreply.github.com>
2026-09-16 23:40:48 +0800

    Load Coach Kyle production cleanup patch

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ document.write('<script src="config.js?v=coachkyle1"><\/script>');
 document.write('<script src="template-runtime.js?v=template130"><\/script>');
 document.write('<script src="legacy-script.js?v=template130"><\/script>');
 document.write('<script src="template-public-patches.js?v=template130"><\/script>');
-document.write('<script src="coach-kyle-client.js?v=coachkyle1"><\/script>');
+document.write('<script src="coach-kyle-client.js?v=coachkyle2"><\/script>');
+document.write('<script src="coach-kyle-hotfix.js?v=coachkyle1"><\/script>');
