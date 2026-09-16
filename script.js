9364c64db6fb39550afcd3a2e5cc459195f70247
xbalancedvpn <56534826+xbalancedvpn@users.noreply.github.com>
2026-09-16 23:07:45 +0800

    Publish Coach Kyle site loader

---
+++ script.js
@@ -0,0 +1,6 @@
+// Coach Booking Master Template v1.3.0 public loader.
+document.write('<script src="config.js?v=coachkyle1"><\/script>');
+document.write('<script src="template-runtime.js?v=template130"><\/script>');
+document.write('<script src="legacy-script.js?v=template130"><\/script>');
+document.write('<script src="template-public-patches.js?v=template130"><\/script>');
+document.write('<script src="coach-kyle-client.js?v=coachkyle1"><\/script>');
