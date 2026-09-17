8bdc6545d479e537f6fee3cd62586d199c1c7593
xbalancedvpn <56534826+xbalancedvpn@users.noreply.github.com>
2026-09-17 11:38:25 +0800

    Add Coach Kyle admin loader

---
+++ admin.js
@@ -0,0 +1,6 @@
+// Coach Kyle admin loader.
+document.write('<script src="config.js?v=coachkyle-admin1"><\/script>');
+document.write('<script src="template-runtime.js?v=template130"><\/script>');
+document.write('<script src="legacy-admin.js?v=template130"><\/script>');
+document.write('<script src="template-admin-patches.js?v=template130"><\/script>');
+document.write('<script src="template-admin-extras.js?v=template130"><\/script>');
